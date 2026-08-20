f3831fb7be83aa676e7365ac67dd616f2c0cbdab
İzzet Şahin <izzetsahin86@icloud.com>
2026-08-20 03:41:03 +0300

    Saha butonu alt menü düzenini etkinleştir

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'leaflet/dist/leaflet.css'
 import './styles.css'
+import './field-points-layout.css'
 import App from './App'
 import FieldPointsFeature from './components/FieldPointsFeature'
 

2c628cbd55ec2773a9d8af9a5139f61d6f39f018
İzzet Şahin <izzetsahin86@icloud.com>
2026-08-20 06:34:30 +0300

    Bump cache for coordinate format support

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'evren-gis-v29'
+const CACHE_VERSION = 'evren-gis-v30'
 const SHELL_CACHE = `${CACHE_VERSION}-shell`
 const TILE_CACHE = `${CACHE_VERSION}-tiles`
 const SHELL_FILES = ['/', '/index.html', '/offline.html', '/manifest.webmanifest', '/icons/evren-gis.svg', '/icons/evren-jeofizik-logo.svg']

50cbbf04670cee8f6d171685d6581b1291d9f1b6
İzzet Şahin <izzetsahin86@icloud.com>
2026-09-04 23:31:04 +0300

    Keep admin countdown outside React bundle

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ import './document-coordinate-compact.css'
 import App from './App'
 import AdminSearchAccessFeature from './components/AdminSearchAccessFeature'
 import AdminDeviceHistoryFeature from './components/AdminDeviceHistoryFeature'
-import AdminLoginSecurityStatusFeature from './components/AdminLoginSecurityStatusFeature'
 import CompleteDataCleanupFeature from './components/CompleteDataCleanupFeature'
 import FieldPointsFeatureHost from './components/FieldPointsFeatureHost'
 import FieldPointsTransferInlineFeature from './components/FieldPointsTransferInlineFeature'
@@ -21,7 +20,6 @@ createRoot(document.getElementById('root')!).render(
     <App />
     <AdminSearchAccessFeature />
     <AdminDeviceHistoryFeature />
-    <AdminLoginSecurityStatusFeature />
     <CompleteDataCleanupFeature />
     <FieldPointsFeatureHost />
     <FieldPointsTransferInlineFeature />

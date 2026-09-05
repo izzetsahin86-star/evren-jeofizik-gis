68619010e4883645386cde829ec3dd37d778531e
İzzet Şahin <izzetsahin86@icloud.com>
2026-09-05 15:42:14 +0300

    Mount DES calibration workspace

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,7 @@ import UndergroundModelFeature from './components/UndergroundModelFeature'
 import UndergroundModelV2Feature from './components/UndergroundModelV2Feature'
 import DESAnalysisFeature from './components/DESAnalysisFeature'
 import DESProfessionalFeature from './components/DESProfessionalFeature'
+import DESCalibrationFeature from './components/DESCalibrationFeature'
 
 // Feature modules are mounted independently so existing GIS flows stay isolated.
 createRoot(document.getElementById('root')!).render(
@@ -33,6 +34,7 @@ createRoot(document.getElementById('root')!).render(
     <UndergroundModelV2Feature />
     <DESAnalysisFeature />
     <DESProfessionalFeature />
+    <DESCalibrationFeature />
   </StrictMode>,
 )
 
